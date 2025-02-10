6bda3d6936392c56c052ffe9ac75e287af2dfd0e
ottozip <ottozipx@gmail.com>
2025-02-10 20:18:08 +0900

    feat(#3): Objection Entity 구현

--- src/main/java/com/shiftm/shiftm/domain/objection/domain/Objection.java
+++ src/main/java/com/shiftm/shiftm/domain/objection/domain/Objection.java
@@ -1,4 +1,29 @@
 package com.shiftm.shiftm.domain.objection.domain;
 
+import com.shiftm.shiftm.domain.objection.domain.enums.Status;
+import com.shiftm.shiftm.domain.objection.domain.enums.Type;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "objections")
+@Entity
 public class Objection {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
 }
