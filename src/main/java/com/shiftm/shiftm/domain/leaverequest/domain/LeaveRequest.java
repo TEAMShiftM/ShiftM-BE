1b24d7e3e08ff2954e18e9580d5d84931edf1712
ottozip <ottozipx@gmail.com>
2025-02-10 20:11:50 +0900

    feat(#3): LeaveRequest Entity 구현

--- src/main/java/com/shiftm/shiftm/domain/leaverequest/domain/LeaveRequest.java
+++ src/main/java/com/shiftm/shiftm/domain/leaverequest/domain/LeaveRequest.java
@@ -1,4 +1,29 @@
 package com.shiftm.shiftm.domain.leaverequest.domain;
 
+import com.shiftm.shiftm.domain.leaverequest.domain.enums.Status;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "leave_requests")
+@Entity
 public class LeaveRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
 }
---
+++ src/main/java/com/shiftm/shiftm/domain/leaverequest/domain/enums/Status.java
@@ -0,0 +1,8 @@
+package com.shiftm.shiftm.domain.leaverequest.domain.enums;
+
+public enum Status {
+    PENDING,
+    APPROVED,
+    REJECTED,
+    CANCELED;
+}
