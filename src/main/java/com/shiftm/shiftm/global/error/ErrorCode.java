83c801f7a06f589489766ef060e26712d1dd21b4
ottozip <ottozipx@gmail.com>
2025-02-11 23:29:24 +0900

    feat(#7): Common ErrorHandler 구현

---
+++ src/main/java/com/shiftm/shiftm/global/error/ErrorCode.java
@@ -0,0 +1,19 @@
+package com.shiftm.shiftm.global.error;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    /* COMMON ERROR */
+    INTERNAL_SERVER_ERROR(500, "COMMON001", "Internal Server Error");
+
+    private final int status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(final int status, final String code, final String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}
