cd19b6124ac9a59a78ab0fdaf480cc38b51198e2
ottozip <ottozipx@gmail.com>
2025-02-11 23:15:00 +0900

    feat(#7): BusinessException 구현

---
+++ src/main/java/com/shiftm/shiftm/global/error/BusinessException.java
@@ -0,0 +1,13 @@
+package com.shiftm.shiftm.global.error;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public BusinessException(final ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
