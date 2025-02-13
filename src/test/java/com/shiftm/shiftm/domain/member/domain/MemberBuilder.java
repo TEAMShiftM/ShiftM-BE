924a2823cefe9886e3937b5c504e04cb15cd826b
ottozip <ottozipx@gmail.com>
2025-02-13 18:49:26 +0900

    feat(#9): MemberService signUp() 테스트 구현

---
+++ src/test/java/com/shiftm/shiftm/domain/member/domain/MemberBuilder.java
@@ -0,0 +1,31 @@
+package com.shiftm.shiftm.domain.member.domain;
+
+import com.shiftm.shiftm.domain.member.domain.enums.Gender;
+import com.shiftm.shiftm.domain.member.domain.enums.Role;
+import com.shiftm.shiftm.domain.member.domain.enums.Status;
+
+import java.time.LocalDate;
+
+public class MemberBuilder {
+    public static Member build() {
+        final String id = "shiftm";
+        final String password = "Password!0";
+        final String email = "shiftm@gmail.com";
+        final String name = "김사원";
+        final LocalDate birthDate = LocalDate.of(2000, 1, 1);
+        final Gender gender = Gender.FEMALE;
+
+        {
+            return Member.builder()
+                    .id(id)
+                    .password(password)
+                    .email(email)
+                    .name(name)
+                    .birthDate(birthDate)
+                    .gender(gender)
+                    .status(Status.ACTIVE)
+                    .role(Role.USER)
+                    .build();
+        }
+    }
+}
---
+++ src/test/java/com/shiftm/shiftm/domain/member/service/MemberServiceTest.java
@@ -0,0 +1,83 @@
+package com.shiftm.shiftm.domain.member.service;
+
+import com.shiftm.shiftm.domain.member.domain.Member;
+import com.shiftm.shiftm.domain.member.domain.MemberBuilder;
+import com.shiftm.shiftm.domain.member.dto.SignUpRequestBuilder;
+import com.shiftm.shiftm.domain.member.dto.request.SignUpRequest;
+import com.shiftm.shiftm.domain.member.dto.response.MemberResponse;
+import com.shiftm.shiftm.domain.member.exception.DuplicatedEmailException;
+import com.shiftm.shiftm.domain.member.exception.DuplicatedIdException;
+import com.shiftm.shiftm.domain.member.repository.MemberRepository;
+import com.shiftm.shiftm.test.UnitTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class MemberServiceTest extends UnitTest {
+    @InjectMocks
+    private MemberService memberService;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Spy
+    PasswordEncoder passwordEncoder;
+    private Member member;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        member = MemberBuilder.build();
+    }
+
+    @Test
+    public void 회원가입_성공() {
+        // given
+        final SignUpRequest requestDto = SignUpRequestBuilder.build(member);
+
+        when(memberRepository.existsById(any())).thenReturn(false);
+        when(memberRepository.existsByEmail(any())).thenReturn(false);
+        when(memberRepository.save(any())).thenReturn(member);
+
+        // when
+        final MemberResponse responseDto = memberService.signUp(requestDto);
+
+        // then
+        assertThat(responseDto).isNotNull();
+        assertThat(responseDto.id()).isEqualTo(member.getId());
+        assertThat(responseDto.email()).isEqualTo(member.getEmail());
+        assertThat(responseDto.name()).isEqualTo(member.getName());
+        assertThat(responseDto.birthDate()).isEqualTo(member.getBirthDate());
+        assertThat(responseDto.gender()).isEqualTo(member.getGender());
+    }
+
+    @Test
+    public void 회원가입_실패_아이디_중복() {
+        // given
+        final SignUpRequest requestDto = SignUpRequestBuilder.build(member);
+
+        when(memberRepository.existsById(any())).thenReturn(true);
+
+        // when, then
+        assertThrows(DuplicatedIdException.class, () -> memberService.signUp(requestDto));
+    }
+
+    @Test
+    public void 회원가입_실패_이메일_중복() {
+        // given
+        final SignUpRequest requestDto = SignUpRequestBuilder.build(member);
+
+        when(memberRepository.existsById(any())).thenReturn(false);
+        when(memberRepository.existsByEmail(any())).thenReturn(true);
+
+        // when, then
+        assertThrows(DuplicatedEmailException.class, () -> memberService.signUp(requestDto));
+    }
+}
