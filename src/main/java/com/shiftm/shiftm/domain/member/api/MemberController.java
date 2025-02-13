c3eb056a6947173a8d5681081877aa8ae842529e
ottozip <ottozipx@gmail.com>
2025-02-13 18:50:17 +0900

    feat(#9): MemberController 수정

--- src/main/java/com/shiftm/shiftm/domain/member/api/MemberController.java
+++ src/main/java/com/shiftm/shiftm/domain/member/api/MemberController.java
@@ -3,8 +3,10 @@ package com.shiftm.shiftm.domain.member.api;
 import com.shiftm.shiftm.domain.member.dto.request.SignUpRequest;
 import com.shiftm.shiftm.domain.member.dto.response.MemberResponse;
 import com.shiftm.shiftm.domain.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +17,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public MemberResponse signUp(final SignUpRequest requestDto) {
+    public MemberResponse signUp(@Valid @RequestBody final SignUpRequest requestDto) {
         return memberService.signUp(requestDto);
     }
 }
