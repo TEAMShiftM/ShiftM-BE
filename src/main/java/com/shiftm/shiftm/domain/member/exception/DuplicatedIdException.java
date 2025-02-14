470f0be1dbf63877770175f5fa911747348af553
ottozip <ottozipx@gmail.com>
2025-02-14 21:49:20 +0900

    feat(#9): DuplicatedIdException 수정

--- src/main/java/com/shiftm/shiftm/domain/member/exception/DuplicatedIdException.java
+++ src/main/java/com/shiftm/shiftm/domain/member/exception/DuplicatedIdException.java
@@ -1,9 +1,9 @@
 package com.shiftm.shiftm.domain.member.exception;
 
 import com.shiftm.shiftm.global.error.ErrorCode;
-import com.shiftm.shiftm.global.error.exception.BusinessException;
+import com.shiftm.shiftm.global.error.exception.InvalidValueException;
 
-public class DuplicatedIdException extends BusinessException {
+public class DuplicatedIdException extends InvalidValueException {
     public DuplicatedIdException(final String id) {
         super(id, ErrorCode.DUPLICATED_ID);
     }
