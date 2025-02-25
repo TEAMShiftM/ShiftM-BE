746daff3be0a2b5042a6c04a60ac3d46b00eac04
yeon2lee <md04254@naver.com>
2025-02-25 22:05:22 +0900

    feat(#14): 회사 정보 수정 및 조회 테스트 구현

--- src/test/java/com/shiftm/shiftm/domain/company/service/CompanyServiceTest.java
+++ src/test/java/com/shiftm/shiftm/domain/company/service/CompanyServiceTest.java
@@ -4,6 +4,7 @@ import com.shiftm.shiftm.domain.company.domain.Company;
 import com.shiftm.shiftm.domain.company.domain.CompanyBuilder;
 import com.shiftm.shiftm.domain.company.dto.CompanyRequestBuilder;
 import com.shiftm.shiftm.domain.company.dto.request.CompanyRequest;
+import com.shiftm.shiftm.domain.company.exception.CompanyNotFoundException;
 import com.shiftm.shiftm.domain.company.exception.DuplicatedCompanyIdException;
 import com.shiftm.shiftm.domain.company.repository.CompanyRepository;
 import com.shiftm.shiftm.test.UnitTest;
@@ -12,6 +13,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import java.time.LocalTime;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -64,5 +68,82 @@ class CompanyServiceTest extends UnitTest {
 
         //when, then
         assertThrows(DuplicatedCompanyIdException.class, () -> companyService.createCompany(requestDto));
+    }
+
+    @DisplayName("회사 수정 성공")
+    @Test
+    void 회사_수정_성공() {
+        //given
+        final CompanyRequest requestDto = new CompanyRequest("shift", LocalTime.of(0, 0), LocalTime.of(0, 0),
+                LocalTime.of(0, 0), LocalTime.of(0, 0), 0.0, 0.0);
+        when(companyRepository.findById(any())).thenReturn(Optional.of(company));
+
+        //when
+        final Company updateCompany = companyService.updateCompany(null, requestDto);
+
+        //then
+        assertThat(updateCompany).isNotNull();
+        assertThat(updateCompany.getCompanyId()).isEqualTo("shift");
+        assertThat(updateCompany.getCheckinTime()).isEqualTo(LocalTime.of(0, 0));
+        assertThat(updateCompany.getCheckoutTime()).isEqualTo(LocalTime.of(0, 0));
+        assertThat(updateCompany.getBreakStartTime()).isEqualTo(LocalTime.of(0, 0));
+        assertThat(updateCompany.getBreakEndTime()).isEqualTo(LocalTime.of(0, 0));
+        assertThat(updateCompany.getLatitude()).isEqualTo(0.0);
+        assertThat(updateCompany.getLongitude()).isEqualTo(0.0);
+    }
+
+    @DisplayName("회사 수정 실패 - 이름 중복")
+    @Test
+    public void 회사_수정_실패_이름_중복 () {
+        //given
+        final CompanyRequest requestDto = new CompanyRequest("shiftm", LocalTime.of(0, 0), LocalTime.of(0, 0),
+                LocalTime.of(0, 0), LocalTime.of(0, 0), 0.0, 0.0);
+        when(companyRepository.findById(any())).thenReturn(Optional.of(company));
+        when(companyRepository.existsByCompanyId(any())).thenReturn(true);
+
+        //when, then
+        assertThrows(DuplicatedCompanyIdException.class, () -> companyService.updateCompany(null, requestDto));
+    }
+
+    @DisplayName("회사 수정 실패 - 존재하지 않는 회사ID")
+    @Test
+    public void 회사_수정_실패_존재하지_않는_회사ID() {
+        //given
+        final CompanyRequest requestDto = new CompanyRequest("shift", LocalTime.of(0, 0), LocalTime.of(0, 0),
+                LocalTime.of(0, 0), LocalTime.of(0, 0), 0.0, 0.0);
+        when(companyRepository.findById(any())).thenReturn(Optional.empty());
+
+        //when, then
+        assertThrows(CompanyNotFoundException.class, () -> companyService.updateCompany(100L, requestDto));
+    }
+
+    @DisplayName("회사 조회 성공")
+    @Test
+    public void 회사_조회_성공() {
+        //given
+        when(companyRepository.findById(any())).thenReturn(Optional.of(company));
+
+        //when
+        final Company getCompany = companyService.getCompany(null);
+
+        //then
+        assertThat(getCompany).isNotNull();
+        assertThat(getCompany.getCompanyId()).isEqualTo(company.getCompanyId());
+        assertThat(getCompany.getCheckinTime()).isEqualTo(company.getCheckinTime());
+        assertThat(getCompany.getCheckoutTime()).isEqualTo(company.getCheckoutTime());
+        assertThat(getCompany.getBreakStartTime()).isEqualTo(company.getBreakStartTime());
+        assertThat(getCompany.getBreakEndTime()).isEqualTo(company.getBreakEndTime());
+        assertThat(getCompany.getLatitude()).isEqualTo(company.getLatitude());
+        assertThat(getCompany.getLongitude()).isEqualTo(company.getLongitude());
+    }
+
+    @DisplayName("회사 조회 실패 - 존재하지 않는 회사ID")
+    @Test
+    public void 회사_조회_실패_존재하지_않는_회사ID() {
+        //given
+        when(companyRepository.findById(any())).thenReturn(Optional.empty());
+
+        //when, then
+        assertThrows(CompanyNotFoundException.class, () -> companyService.getCompany(null));
     }
 }
