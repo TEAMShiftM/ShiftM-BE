bc46edb0910bf8f326a622fa1fa81f51d83fc88a
jiruen <kjr2699@naver.com>
2025-02-26 23:51:42 +0900

    setting(#22): Swagger 환경 설정

---
+++ src/main/java/com/shiftm/shiftm/global/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.shiftm.shiftm.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("ShiftM API")
+                        .version("v0.0.1")
+                        .description("ShiftM API 명세서입니다."))
+                .components(new Components()
+                        .addSecuritySchemes("Authorization",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)))
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"));
+    }
+}
