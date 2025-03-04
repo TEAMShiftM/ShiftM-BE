ac7d610bbe6b0a93c0f6ae2fcf55dc14e4fa4dfd
yeon2lee <md04254@naver.com>
2025-03-05 03:37:30 +0900

    refactor(#29): 페이징 요청 파라미터 @PageableDefault 적용

--- src/main/java/com/shiftm/shiftm/domain/shift/api/AdminShiftController.java
+++ src/main/java/com/shiftm/shiftm/domain/shift/api/AdminShiftController.java
@@ -13,14 +13,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,28 +32,24 @@ public class AdminShiftController {
 
     // 전체 근무 기록 조회
     @GetMapping
-    public AdminShiftListResponse getShifts(@RequestParam(defaultValue = "0") final int page,
-                                            @RequestParam(defaultValue = "10") final int size,
+    public AdminShiftListResponse getShifts(@PageableDefault Pageable pageable,
                                             @RequestParam(required = false) final String name) {
-        Pageable pageable = PageRequest.of(page, size);
         Page<Shift> shiftPage = shiftService.getShifts(pageable, name);
         List<AdminShiftResponse> shifts = shiftPage.getContent().stream()
                 .map(shift -> new AdminShiftResponse(shift))
                 .collect(Collectors.toList());
-        return new AdminShiftListResponse(shifts, page, size, shiftPage.getTotalPages(), shiftPage.getTotalElements());
+        return new AdminShiftListResponse(shifts, shiftPage.getNumber(), shiftPage.getSize(), shiftPage.getTotalPages(), shiftPage.getTotalElements());
     }
 
     // 사후 출근 신청 조회
     @GetMapping("/after-checkin")
-    public AfterCheckinListResponse getAfterCheckin(@RequestParam(defaultValue = "0") final int page,
-                                                    @RequestParam(defaultValue = "10") final int size,
+    public AfterCheckinListResponse getAfterCheckin(@PageableDefault Pageable pageable,
                                                     @RequestParam(required = false) final String name) {
-        Pageable pageable = PageRequest.of(page, size);
         Page<Shift> shiftPage = shiftService.getAfterCheckin(pageable, name);
         final List<AfterCheckinResponse> shifts = shiftPage.getContent().stream()
                 .map(shift -> new AfterCheckinResponse(shift))
                 .collect(Collectors.toList());
-        return new AfterCheckinListResponse(shifts, page, size, shiftPage.getTotalPages(), shiftPage.getTotalElements());
+        return new AfterCheckinListResponse(shifts, shiftPage.getNumber(), shiftPage.getSize(), shiftPage.getTotalPages(), shiftPage.getTotalElements());
     }
 
     // 출근 상태 변경
