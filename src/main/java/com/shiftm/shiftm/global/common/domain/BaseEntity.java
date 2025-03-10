eda632cd0fb29859f882a27a8b67f0fe38212cdd
jiruen <kjr2699@naver.com>
2025-03-10 15:10:59 +0900

    feat(#41): BaseEntity를 추상 클래스로 수정

--- src/main/java/com/shiftm/shiftm/global/common/domain/BaseEntity.java
+++ src/main/java/com/shiftm/shiftm/global/common/domain/BaseEntity.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseEntity {
+public abstract class BaseEntity {
     @CreatedDate
     @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
