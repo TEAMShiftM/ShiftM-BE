3987552374768e35b30a6dc2f9eef18f97b3f1f4
yeon2lee <md04254@naver.com>
2025-03-13 20:16:03 +0900

    chore(#51): DistanceUtil의 변수명 수정

--- src/main/java/com/shiftm/shiftm/global/util/DistanceUtil.java
+++ src/main/java/com/shiftm/shiftm/global/util/DistanceUtil.java
@@ -4,7 +4,7 @@ public class DistanceUtil {
 
     private DistanceUtil() { }
 
-    private static final double EARTH_RADIUS_KM = 6371000;
+    private static final double EARTH_RADIUS = 6371000;
 
     public static double calculateDistance(final double originLatitude, final double originLongitude, final double destinationLatitude, final double detinationLongitude) {
         final double latitudeDifference = Math.toRadians(destinationLatitude - originLatitude);
@@ -14,6 +14,6 @@ public class DistanceUtil {
                         Math.sin(longitudeDifference / 2) * Math.sin(longitudeDifference / 2);
         final double centralAngle = 2 * Math.atan2(Math.sqrt(haversinePart), Math.sqrt(1 - haversinePart));
 
-        return EARTH_RADIUS_KM * centralAngle;
+        return EARTH_RADIUS * centralAngle;
     }
 }
